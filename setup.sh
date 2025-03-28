de2782fccad6c815047452413b2d24915c709c55
shin-sui <shinsaku.su@gmail.com>
2025-03-28 14:46:45 +0900

    Update docker enviroment

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eu
+
+ENV_FILE_PATH=./docker/.env
+USERNAME_VALUE=${USERNAME:-user0}
+USER_ID=1000
+GROUP_ID=1000
+
+if [[ -f ${ENV_FILE_PATH} ]]; then
+	rm ${ENV_FILE_PATH}
+fi
+
+{
+    echo "USERNAME=${USERNAME_VALUE}"
+    echo "UID=$USER_ID"
+    echo "GID=$GROUP_ID"
+} >> ${ENV_FILE_PATH}
+
+
+echo "Environment setup is complete!"
